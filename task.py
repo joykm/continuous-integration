5fb6bba02a3d445d856b17eeebfce2354c3885f5
Kevin Joy <joykm88@gmail.com>
2021-02-25 23:51:02 -0500

    added test and functionality for converting float string to float

--- task.py
+++ task.py
@@ -4,12 +4,26 @@ def my_func():
 
 def conv_num(num_str):
 
+    dec_point_found = False
+    dec_len = 0
+    integer = 0
+    decimal = 0
     result = 0
 
-    # convert int string to int by using the ascii code of the char
-    # subtracted from the ascii code for 0. Each iteration multiply
-    # by 10 to move the decimal place over.
-    for char in num_str:
-        result = result * 10 + ord(char) - ord('0')
+    for i in range(len(num_str)):
+
+        if num_str[i] == ".":
+            dec_point_found = True
+            dec_len = len(num_str) - (i + 1)
+            continue
+
+        if dec_point_found == False:
+            integer = integer * 10 + ord(num_str[i]) - ord('0')
+        else:
+            decimal = decimal * 10 + ord(num_str[i]) - ord('0')
+
+    decimal = decimal / (10 ** dec_len)
+    result = integer + decimal
 
     return result
+
