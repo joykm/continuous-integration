99c0880e88f29e7c98b154951fa124faae1d4714
Kevin Joy <joykm88@gmail.com>
2021-02-25 22:24:12 -0500

    minimal changes to make function 2 testcases pass

--- task.py
+++ task.py
@@ -2,4 +2,13 @@ def my_func():
 	return "Hello World"
 
 def conv_num(num_str):
-	return num_str
+
+	result = 0
+
+	# convert int string to int by using the ascii code of the char
+	# subtracted from the ascii code for 0. Each iteration multiply
+	# by 10 to move the decimal place over.
+	for char in num_str:
+		result = result * 10 + ord(char) - ord('0')
+
+	return result
