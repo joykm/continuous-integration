9f78b34d4b2e85dd9f4158612d364e98a58056d5
Kevin Joy <joykm88@gmail.com>
2021-02-25 21:55:21 -0500

    initiate function 2 and function 2 unit tests

--- task.py
+++ task.py
@@ -1,2 +1,5 @@
 def my_func():
 	return "Hello World"
+
+def conv_num(num_str):
+	return num_str
--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import unittest
-
+from task import *
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
-
+    # function 2 unit tests
+    def test1_func2(self):
+        input = "1234"
+        self.assertTrue(conv_num(input) == 1234)
 
 if __name__ == '__main__':
     unittest.main()
