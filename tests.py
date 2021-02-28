bfd4aba0e6e005e783eee87771cc244412da9ee9
natthaphongkongkaew <kongkaen@oregonstate.edu>
2021-02-27 20:41:58 -0800

    made a test to test where "-" appear somewhere else

--- tests.py
+++ tests.py
@@ -7,14 +7,14 @@ class TestCase(unittest.TestCase):
     # test that an integer string returns as an integer
     def test1_func1(self):
         """test that an integer string returns as an integer"""
-        input = "1234"
-        self.assertTrue(conv_num(input) == 1234)
+        str_num = "1234"
+        self.assertTrue(conv_num(str_num) == 1234)
 
     # test that a float string returns as a float
     def test2_func1(self):
         """test that a float string returns as a float"""
-        input = "1234.12"
-        self.assertTrue(conv_num(input) == 1234.12)
+        str_num = "1234.12"
+        self.assertTrue(conv_num(str_num) == 1234.12)
 
     # test that empty string returns None
     def test3_func1(self):
@@ -28,17 +28,11 @@ class TestCase(unittest.TestCase):
         str_num = "-6.50"
         self.assertEqual(conv_num(str_num), -6.50)
 
-    # test number start with "."
+    # test that negative number returns negative number
     def test5_func1(self):
-        """test that number started with "." will return in proper format"""
-        str_num = ".45"
-        self.assertEqual(conv_num(str_num), 0.45)
-
-    # test number end with "."
-    def test6_func1(self):
-        """test that number end with "." returns in proper format"""
-        str_num = "123."
-        self.assertEqual(conv_num(str_num), 123.0)
+        """test that negative number returns negative number"""
+        str_num = "6.-50"
+        self.assertEqual(conv_num(str_num), None)
 
 
 if __name__ == '__main__':
