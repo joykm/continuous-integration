e8b85cb35756ac17ca642a0b0512c83848e7d4ed
natthaphongkongkaew <kongkaen@oregonstate.edu>
2021-02-27 19:00:12 -0800

    Updated func1 to handle negative number. it pass the test

--- task.py
+++ task.py
@@ -14,6 +14,14 @@ def conv_num(num_str):
     decimal = 0
     result = 0
 
+    # check if input is a negative value
+    # replace "-" with "" (empty character)
+    is_negative = False
+    for i in num_str:
+        if i == "-":
+            num_str = num_str.replace('-', "")
+            is_negative = True
+
     for i in range(len(num_str)):
 
         if num_str[i] == ".":
@@ -27,6 +35,10 @@ def conv_num(num_str):
             decimal = decimal * 10 + ord(num_str[i]) - ord('0')
 
     decimal = decimal / (10 ** dec_len)
-    result = integer + decimal
+
+    if is_negative:
+        result = (integer + decimal) * (-1)
+    else:
+        result = integer + decimal
 
     return result
